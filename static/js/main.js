b324581b1990d37b8f7f87759502df9836917b6d
Safwan Samsudeen <safwan@Safwans-MacBook-Air.local>
2020-09-22 18:47:25 +0530

    Finished homepage with responsiveness, added navbar and footer to menu page, have to add items and styling.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
-// const date = new Date();
-// document.querySelector('.year').innerHTML = date.getFullYear();
+$(function () {
+    const date = new Date();
+    document.querySelector('.year').innerHTML = date.getFullYear();
+    $('.nav-item.active').css('box-shadow', 'inset 0 -3px 0 0 #000')
+})
 
