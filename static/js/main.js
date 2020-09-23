73f085e38cc109bc615c7815a6e8f896468018c6
Safwan Samsudeen <safwan@Safwans-MacBook-Air.local>
2020-09-23 18:02:40 +0530

    Almost finished Menu page, little more responsiveness needed, overall responsiveness improved.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
     const date = new Date();
     document.querySelector('.year').innerHTML = date.getFullYear();
-    $('.nav-item.active').css('box-shadow', 'inset 0 -3px 0 0 #000')
+    $('.nav-item.active').css('box-shadow', 'inset 0 -3px 0 0 #000');
+    $('.form-control').on('keyup', e => {
+        const $unavailable = $(".unavailable")
+        $unavailable.hide();
+        $(".item").hide();
+        console.log(e.target.value.trim().toLowerCase())
+        const results = $(".item:contains(" + e.target.value.trim().toLowerCase() + ")");
+        results.length === 0 ? $unavailable.show() : results.show();
+    })
 })
 
