9e5a5e1b71a1f50887f7b57e23105594e8d9d0f8
Safwan Samsudeen <safwan@Safwans-MacBook-Air.local>
2020-09-25 08:58:48 +0530

    Finished card and search functionality in menu page.  Register and Login pages done.

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,24 @@ $(function () {
     $('.nav-item.active').css('box-shadow', 'inset 0 -3px 0 0 #000');
     $('.form-control').on('keyup', e => {
         const $unavailable = $(".unavailable")
+        const $rows = $('.row')
+        const $items = $(".item");
+        let $results = [];
+        $rows.show();
         $unavailable.hide();
-        $(".item").hide();
-        console.log(e.target.value.trim().toLowerCase())
-        const results = $(".item:contains(" + e.target.value.trim().toLowerCase() + ")");
-        results.length === 0 ? $unavailable.show() : results.show();
+        $items.hide();
+        $items.each((i, node) => node.textContent.toLowerCase().includes(e.target.value) ? $results.push(node) : null)
+        $results = $($results);
+        if ($results.length === 0) {
+            $rows.hide();
+            $unavailable.show();
+        } else {
+            $results.show();
+            $rows.each((i, node) => {
+                $node = $(node)
+                if($node.children(':visible').length === 1) $node.hide()
+            })
+        }
     })
 })
 
