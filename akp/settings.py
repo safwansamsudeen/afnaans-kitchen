66a396fd5ea4f6657b2989dc7c0e476deb8a2397
Safwan Samsudeen <62411302+safwan-samsudeen@users.noreply.github.com>
2021-01-08 15:57:54 +0530

    Update settings.py

--- akp/settings.py
+++ akp/settings.py
@@ -140,5 +140,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'afnaanskitchen.team@gmail.com'
-EMAIL_HOST_PASSWORD = 'AfnaansK@ak13'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_USE_TLS = True
